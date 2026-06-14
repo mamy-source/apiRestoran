8b05433732b599f7329e4505e9d8e47005a69b53
Mamy <mamitianarandrianantenaina253@gmail.com>
2026-06-14 15:01:59 +0300

    comments

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,16 +15,14 @@ import { uploadSingle } from '../middlewares/upload.middleware.js';
 
 const router = Router();
 
-// ==================== Routes protégées (authentification requise) ====================
 router.use(protect);
 
-// ==================== User (ny tenany ihany) ====================
+//user profile routes (authenticated users only)
 router.get('/profile', getProfile);
 router.put('/profile', uploadSingle('users', 'avatar'), updateProfile);
 router.delete('/profile/avatar', deleteAvatar);
 
-// ==================== ADMIN ONLY ====================
-// Ny ADMIN ihany no afaka miditra amin'ireto routes ireto
+//Admin only
 router.use(restrictTo('ADMIN'));
 
 router.get('/', getAllUsers);
